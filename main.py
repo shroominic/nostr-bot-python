be119afdc410cdd2aa2dbf28646833e37285aba2
Shroominic <db2002dominic@gmail.com>
2025-09-05 13:23:25 +0100

    activation contition

--- main.py
+++ main.py
@@ -1,51 +1,42 @@
 import asyncio
 import os
-from crypto import (
-    generate_ephemeral_keypair,
-    encode_npub,
-    decode_nsec_to_hex,
-    priv_to_pub_hex,
-)
+from crypto import encode_npub, init_identity
 from nostr import stream_nostr_messages, reply_to_message
 from ai import generate_ai_response
 
 relays = [
     "wss://relay.damus.io",
     "wss://relay.primal.net",
-    "wss://nos.lol",
-    "wss://offchain.pub",
-    "wss://nostr21.com",
     "wss://pyramid.fiatjaf.com/",
-    "wss://relay.sig.fun",
+    "wss://relay.nostr.band/",
+    "wss://nos.lol",
 ]
 
 nsec = os.getenv("NSEC")
-bot_identity = (
-    (decode_nsec_to_hex(nsec), priv_to_pub_hex(decode_nsec_to_hex(nsec)))
-    if nsec
-    else generate_ephemeral_keypair()
-)
+privkey, pubkey = init_identity(nsec)
 activation_cmd = os.getenv("ACTIVATION_CMD", "!robot")
-print(f"Bot started with identity: {encode_npub(bot_identity[1])}")
+
+print(f"Bot started with identity: {encode_npub(pubkey)}")
+
+
+def activation_condition(message: dict) -> bool:
+    print(
+        f"Message received from {encode_npub(message['pubkey'])}: {message['content'][:50]}{'...' if len(message['content']) > 50 else ''}"
+    )
+    return message["content"].startswith(activation_cmd) or pubkey in message["content"]
 
 
 async def main():
     async for message in stream_nostr_messages(
-        relays=relays,
-        filters=[{"kinds": [1]}],
-        since_seconds=60 * 10,
+        relays=relays, filters=[{"kinds": [1]}], since_seconds=1
     ):
-        if activation_cmd in message["content"]:
-            print(
-                f"Processing message from {encode_npub(message['pubkey'])}: {message['content'][:50]}{'...' if len(message['content']) > 50 else ''}"
-            )
-
+        if activation_condition(message):
             response = await generate_ai_response(
                 message["content"],
             )
             await reply_to_message(
                 relays,
-                bot_identity,
+                (privkey, pubkey),
                 message,
                 response,
             )
